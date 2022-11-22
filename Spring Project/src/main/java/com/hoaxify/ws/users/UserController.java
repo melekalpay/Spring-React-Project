20e4c2c98d202af839bde55f04c6c665d68b1e68
melekalpay <melekalpy7@gmail.com>
2022-11-22 18:55:59 +0300

    axios eklendi

--- Spring Project/src/main/java/com/hoaxify/ws/users/UserController.java
+++ Spring Project/src/main/java/com/hoaxify/ws/users/UserController.java
@@ -1,6 +1,7 @@
 package com.hoaxify.ws.users;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.slf4j.Logger;
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    @CrossOrigin
     @PostMapping("api/1.0/users")
     public void createuser(@RequestBody Users users){
         logger.info(users.toString());
